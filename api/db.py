136f2369a3ee816145a108860b3c09b46e8d19f7
Zapodask <joaopedroemerim@gmail.com>
2021-12-17 20:38:40 -0300

    cron

--- api/db.py
+++ api/db.py
@@ -1,8 +1,45 @@
 from pymongo import MongoClient
+import requests
+import json
 
 
 client = MongoClient(
-    "mongodb+srv://zapodask:zapodask@spacecluster.kngaq.mongodb.net/spacedb?retryWrites=true&w=majority"
+    "mongodb+srv://zapodask:zapodask@cluster0.kngaq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 )
 
 db = client["spacedb"]
+
+articles = db["articles"]
+
+
+def updateDB():
+    to_add = []
+    n = 0
+    loop = True
+
+    while loop:
+        res = requests.get(
+            f"https://api.spaceflightnewsapi.net/v3/articles?_start={n * 10}"
+        )
+        text = json.loads(res.text)
+
+        for item in list(text):
+            article = articles.find_one({"title": item.get("title")})
+
+            if article == None:
+                to_add.append(item)
+                print(f'Appending: {item.get("title")}')
+            else:
+                tal = len(to_add)
+
+                if tal != 0:
+                    print(f"{tal} articles to add")
+
+                    articles.insert_many(to_add)
+                else:
+                    print("No articles to add")
+
+                loop = False
+                break
+
+        n += 1
--- api/index.py
+++ api/index.py
@@ -1,6 +1,22 @@
 from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+
 from app.app import blueprint
+from db import updateDB
 
 app = Flask(__name__)
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(
+    id="Update db",
+    func=updateDB,
+    trigger="cron",
+    hour="9",
+    minute="0",
+    second="0",
+)
+scheduler.start()
+
+
 app.register_blueprint(blueprint)
 app.run()
